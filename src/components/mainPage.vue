0822d6286523ff0262a3b39cd1f4830ab0de9c6a
Patricia Ramos <patriciaramosf94@gmail.com>
2021-04-01 12:29:55 +0200

    project init

---
+++ src/components/mainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mainPage">
+    holaaa
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.mainPage{
+  background-color:red;
+}
+</style>
