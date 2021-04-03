896c5c083689e0487af9c70447337b1ad34f4b31
Patricia Ramos <patriciaramosf94@gmail.com>
2021-04-03 13:38:25 +0200

    still circle shape

---
+++ src/components/childComponents/childHolidaysCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="child childHolidaysCard">
+    <h2 class="childHolidaysCard_date">{{ holidaysDate }}</h2>
+    <div class="childHolidaysCard_dateCartoon">
+      <div class="month_date">
+        {{ holidaysMonth }}
+      </div>
+      <div class="day_date">
+        {{ holidaysDay }}
+      </div>
+    </div>
+    <p class="childHolidaysCard_location text__secondary">
+      {{ holidaysLocation }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "childHolidaysCard",
+  props: {
+    holidaysDate: String,
+    holidaysMonth: String,
+    holidaysDay: String,
+    holidaysLocation: String,
+  },
+};
+</script>
+<style>
+.childHolidaysCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.childHolidaysCard_date {
+  font-size: 14px;
+}
+.childHolidaysCard_dateCartoon {
+  border-radius: 50%;
+  border: 1px solid grey;
+  width: 50px;
+  height: 50px;
+  background-color: #ebeff2;
+}
+.month_date {
+  background-color: #f57c05;
+  color: white;
+  shape-outside: circle(50% at 50% 50%);
+}
+.day_date {
+    shape-outside: circle(50% at 50% 50%);
+}
+</style>
--- src/components/holidaysCard.vue
+++ src/components/holidaysCard.vue
@@ -2,31 +2,74 @@
   <div class="item holidaysWrapper">
     <div class="title header">
       <h1 class="header_letters">{{ header.toUpperCase() }}</h1>
+    </div>
+    <div class="body bodyHolidays">
+      <div class="rowHolidays">
+        <childHolidaysCard
+          holidaysDate="San José"
+          holidaysMonth="Mar"
+          holidaysDay="13"
+          holidaysLocation="En Madrid, Barcelona y 3 más"
+        />
+        <childHolidaysCard
+          holidaysDate="San José"
+          holidaysMonth="Mar"
+          holidaysDay="13"
+          holidaysLocation="En Barcelona"
+        />
       </div>
-    <div class="body"></div>
-     <div class="title footer">
+      <div class="rowHolidays">
+        <childHolidaysCard
+          holidaysDate="San José"
+          holidaysMonth="Mar"
+          holidaysDay="13"
+          holidaysLocation="En Madrid, Barcelona y 3 más"
+        />
+        <childHolidaysCard
+          holidaysDate="San José"
+          holidaysMonth="Mar"
+          holidaysDay="13"
+          holidaysLocation="En Barcelona"
+        />
+      </div>
+    </div>
+    <div class="title footer">
       <p class="footer_letters">
-      {{ footer }}
+        {{ footer }}
       </p>
-      <img class="footer_icon" src="../assets/arrow.png"/>
-      </div>
+      <img class="footer_icon" src="../assets/arrow.png" />
+    </div>
   </div>
 </template>
 
 <script>
+import childHolidaysCard from "./childComponents/childHolidaysCard";
 export default {
-  name: 'holidaysCard',
+  name: "holidaysCard",
   props: {
     header: String,
-    footer:String,
-  }
-}
+    footer: String,
+  },
+  components: {
+    childHolidaysCard,
+  },
+};
 </script>
 <style>
-.holidaysWrapper{
+.holidaysWrapper {
   grid-column-start: 1;
   grid-column-end: 5;
-  grid-row-start:5;
-  grid-row-end:9;
+  grid-row-start: 5;
+  grid-row-end: 9;
+}
+.bodyHolidays {
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content: center;
+}
+.rowHolidays {
+  display: flex;
+  flex-direction: row;
 }
 </style>
